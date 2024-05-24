a174fb0fe0eeaaeaa7786ee01dac8afc034144a6
dbaudwns24 <dbaudwns24@gmail.com>
2024-05-24 17:06:21 +0900

    헤더 html x Link 로 가게 수정

--- front/src/component/main/TopNavBar.jsx
+++ front/src/component/main/TopNavBar.jsx
@@ -1,48 +1,48 @@
 import React, { useState } from 'react';
 import { Navbar, Nav, Dropdown } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { Link } from 'react-router-dom';
 
 function TopNavBar({ isShrunk }) {
 
   return (
     <Navbar className={isShrunk ? 'navbar shrink' : 'navbar expand'}>
-      <Navbar.Brand href={'/'}>
-        <div className="logo"></div> {/* 로고 이미지를 사용하는 div */}
-      </Navbar.Brand>
-      <Nav className="mr-auto">
-        <Dropdown>
-          <Dropdown.Toggle variant="transparent" >
-            칵테일
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item href="/ViewPage">칵테일</Dropdown.Item>
-            <Dropdown.Item href="/Ingredient">재료</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-        <Dropdown>
-          <Dropdown.Toggle variant="transparent" >
-            정보
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item href="/history">역사</Dropdown.Item>
-            <Dropdown.Item href="/craft/mensuration">튜토리얼</Dropdown.Item>
-            <Dropdown.Item href="/trendNews">뉴스</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-        <Dropdown>
-          <Dropdown.Toggle variant="transparent" >
-            컨텐츠
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item href="/weather">무작위 칵테일</Dropdown.Item>
-            <Dropdown.Item href="/customcocktail">커스텀 칵테일</Dropdown.Item>
-            <Dropdown.Item href="/taste">취향 조사</Dropdown.Item>
-            <Dropdown.Item href="/mapsearch">칵테일 바 찾기</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Nav>
-    </Navbar>
+  <Navbar.Brand as={Link} to="/">
+    <div className="logo"></div>
+  </Navbar.Brand>
+  <Nav className="mr-auto">
+    <Dropdown>
+      <Dropdown.Toggle variant="transparent" >
+        칵테일
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item as={Link} to="/ViewPage">칵테일</Dropdown.Item>
+        <Dropdown.Item as={Link} to="/Ingredient">재료</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    <Dropdown>
+      <Dropdown.Toggle variant="transparent" >
+        정보
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item as={Link} to="/history">역사</Dropdown.Item>
+        <Dropdown.Item as={Link} to="/craft/mensuration">튜토리얼</Dropdown.Item>
+        <Dropdown.Item as={Link} to="/trendNews">뉴스</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    <Dropdown>
+      <Dropdown.Toggle variant="transparent" >
+        컨텐츠
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item as={Link} to="/weather">무작위 칵테일</Dropdown.Item>
+        <Dropdown.Item as={Link} to="/customcocktail">커스텀 칵테일</Dropdown.Item>
+        <Dropdown.Item as={Link} to="/taste">취향 조사</Dropdown.Item>
+        <Dropdown.Item as={Link} to="/mapsearch">칵테일 바 찾기</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  </Nav>
+</Navbar>
 
   );
 }
