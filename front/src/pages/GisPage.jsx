c0030c5fdd78dd94b561eb01db59b4af2f0b8eda
YMJ <dbaudwns24@gmail.com>
2024-05-26 23:35:39 +0900

    gispage 위치 이동시 재 랜더링

--- front/src/pages/GisPage.jsx
+++ front/src/pages/GisPage.jsx
@@ -6,27 +6,29 @@ import { Link } from 'react-router-dom';
 
 function GisPage() {
   const [places, setPlaces] = useState([]);
+  const [currentPosition, setCurrentPosition] = useState(null);
+
+  const fetchPlaces = async (latitude, longitude) => {
+    try {
+      const response = await fetch(`https://localhost:9092/api/gis/get?latitude=${latitude}&longitude=${longitude}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setPlaces(data.documents || []);
+    } catch (error) {
+      console.error('Failed to fetch places:', error);
+      setPlaces([]);
+    }
+  };
 
   useEffect(() => {
-    const fetchPlaces = async (latitude, longitude) => {
-      try {
-        const response = await fetch(`https://localhost:9092/api/gis/get?latitude=${latitude}&longitude=${longitude}`);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setPlaces(data.documents || []);
-      } catch (error) {
-        console.error('Failed to fetch places:', error);
-        setPlaces([]);
-      }
-    };
-
     const getCurrentLocation = () => {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
           position => {
             const { latitude, longitude } = position.coords;
+            setCurrentPosition({ latitude, longitude });
             fetchPlaces(latitude, longitude);
           },
           error => {
@@ -43,6 +45,16 @@ function GisPage() {
     getCurrentLocation();
   }, []);
 
+  useEffect(() => {
+    if (currentPosition) {
+      fetchPlaces(currentPosition.latitude, currentPosition.longitude);
+    }
+  }, [currentPosition]);
+
+  const handleMarkerClick = (latitude, longitude) => {
+    setCurrentPosition({ latitude, longitude });
+  };
+
   return (
     <BasicLayout>
       <div className="GisContainer">
@@ -50,26 +62,27 @@ function GisPage() {
           <ul>
             {places && places.length > 0 ? (
               places
-                .sort((a, b) => a.distance - b.distance) // place.distance를 기준으로 오름차순 정렬
+                .sort((a, b) => a.distance - b.distance)
                 .map((place, index) => (
                   <Link to={place.place_url} key={index}>
                     <li className="GisPlaceItem">
-                      <p className="GisPlaceName">{place.place_name}</p>
+                      <p style={{ textDecoration: "none"}} className="GisPlaceName">{place.place_name}</p>
                       <p className="GisAddressName">{place.address_name}</p>
-                      <p className="GisPhone">{place.distance}</p>
-                      <p className="GisPhone">{place.phone}</p>
+                      <p className="GisPhone">{place.distance}m</p>
+                      <p className="GisPhone">전화번호: {place.phone}</p>
                     </li>
                   </Link>
                 ))
             ) : (
               <div className="GisPlaceEmpty">
+                <h1>😭</h1>
                 <p>정보가 없습니다.</p>
               </div>
             )}
           </ul>
         </div>
         <div className="GisMapContainer">
-          <MapContainer places={places} />
+          <MapContainer onMarkerClick={handleMarkerClick} />
         </div>
       </div>
     </BasicLayout>
@@ -77,4 +90,3 @@ function GisPage() {
 }
 
 export default GisPage;
-
