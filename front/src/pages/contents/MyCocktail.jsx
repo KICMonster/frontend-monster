55e9ba5fa553bad017eeeca0c8b448a7d18a6b7a
dbaudwns24 <dbaudwns24@gmail.com>
2024-05-29 15:20:18 +0900

    my칵테일 page 수정

--- front/src/pages/contents/MyCocktail.jsx
+++ front/src/pages/contents/MyCocktail.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import BasicLayout from "../../layouts/BasicLayout";
+import '../../component/main/styles/mycocktail.css';
+import { Link } from "react-router-dom";
 
 function MyCocktail() {
   const [title, setTitle] = useState("");
@@ -14,6 +16,7 @@ function MyCocktail() {
   const [error, setError] = useState(null);
 
   const handleInputChange = (id, field, value) => {
+    console.log(`재료 ${id} 필드 ${field}를 값 ${value}로 업데이트합니다.`);
     setIngredients(prevIngredients =>
       prevIngredients.map(ingredient =>
         ingredient.id === id ? { ...ingredient, [field]: value } : ingredient
@@ -23,6 +26,7 @@ function MyCocktail() {
 
   const addIngredient = () => {
     const newId = ingredients.length + 1;
+    console.log(`새로운 재료를 ID ${newId}로 추가합니다.`);
     setIngredients(prevIngredients => [
       ...prevIngredients,
       { id: newId, name: "", amount: "" }
@@ -32,27 +36,28 @@ function MyCocktail() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // Submit logic here
+      console.log("제목:", title, "내용:", description, "재료:", ingredients, "로 폼을 제출합니다.");
+      // 제출 로직 구현
     } catch (error) {
-      console.error("Error:", error);
+      console.error("오류 발생:", error);
       setError(error.message);
     }
   };
 
   return (
     <BasicLayout>
-      <div style={styles.board}>
-        <div style={styles.container}>  
-          <div style={styles.left}>
-            <div style={styles.ingredientSection}>
+      <div className="MyBoard">
+        <div className="MyContainer">
+          <div className="MyLeft">
+            <div className="MyIngredientSection">
               <h2>추가하실 재료</h2>
-              <button type="button" onClick={addIngredient} style={styles.addButton}>
+              <button type="button" onClick={addIngredient} className="MyAddButton">
                 재료 추가하기
               </button>
-              <div style={styles.ingredientsContainer}>
+              <div className="MyIngredientsContainer">
                 {ingredients.map(ingredient => (
-                  <div key={ingredient.id} style={styles.formGroup}>
-                    <div style={styles.ingredientRow}>
+                  <div key={ingredient.id} className="MyFormGroup">
+                    <div className="MyIngredientRow">
                       <input
                         type="text"
                         placeholder={`재료 ${ingredient.id} 이름`}
@@ -60,7 +65,7 @@ function MyCocktail() {
                         onChange={(e) =>
                           handleInputChange(ingredient.id, "name", e.target.value)
                         }
-                        style={styles.ingredientInput}
+                        className="MyIngredientInput"
                       />
                       <input
                         type="text"
@@ -69,7 +74,7 @@ function MyCocktail() {
                         onChange={(e) =>
                           handleInputChange(ingredient.id, "amount", e.target.value)
                         }
-                        style={styles.amountInput}
+                        className="MyAmountInput"
                       />
                     </div>
                   </div>
@@ -77,37 +82,40 @@ function MyCocktail() {
               </div>
             </div>
           </div>
-          <div style={styles.right}>
-            <form onSubmit={handleSubmit} style={styles.form}>
-              <div style={styles.formGroup}>
-                <label style={styles.label}>제목을 입력해주세요</label>
+          <div className="MyRight">
+            <form onSubmit={handleSubmit} className="MyForm">
+              <div className="MyFormGroup">
+                <label className="MyLabel">제목을 입력해주세요</label>
                 <input
                   type="text"
                   value={title}
                   onChange={(e) => setTitle(e.target.value)}
-                  style={styles.input}
+                  className="MyInput"
                 />
               </div>
-              <div style={styles.formGroup}>
-                <label style={styles.label}>내용</label>
+              <div className="MyFormGroup">
+                <label className="MyLabel">내용</label>
                 <textarea
                   value={description}
                   onChange={(e) => setDescription(e.target.value)}
-                  style={styles.textarea}
+                  className="MyTextarea"
                 />
               </div>
-              <div style={styles.formGroup}>
-                <label style={styles.label}>이미지 첨부</label>
-                <input type="file" style={styles.fileInput} />
+              <div className="MyFormGroup">
+                <label className="MyLabel">이미지 첨부</label>
+                <input type="file" className="MyFileInput" />
               </div>
-              <div style={styles.buttonGroup}>
-                <button type="submit" style={styles.submitButton}>
+              <div className="MyButtonGroup">
+                <button type="submit" className="MySubmitButton">
                   등록
                 </button>
-                <button type="button" style={styles.cancelButton}>
-                  취소
-                </button>
+                <Link to={'/customcocktail'}>
+                  <button type="button" className="MyCancelButton">
+                    취소
+                  </button>
+                </Link>
               </div>
+              {error && <p className="MyError">{error}</p>}
             </form>
           </div>
         </div>
@@ -115,125 +123,5 @@ function MyCocktail() {
     </BasicLayout>
   );
 }
-
-const styles = {
-  board: {
-    backgroundColor: "rgba(255, 255, 255, 0.8)", // 흰색 반투명 배경
-    padding: "20px",
-    borderRadius: "20px",
-    maxWidth: "1200px",
-    margin: "0 auto",
-    marginTop: "125px", // 보드를 아래로 내리는 위치 조정
-    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // 보드에 그림자 추가
-  },
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "flex-start",
-  },
-  left: {
-    marginRight: "20px",
-    flex: "1",
-  },
-  right: {
-    flex: "2",
-  },
-  ingredientSection: {
-    padding: "20px",
-    borderRight: "1px solid #ccc",
-    maxHeight: "500px",
-    overflowY: "auto",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  ingredientsContainer: {
-    marginBottom: "20px",
-    width: "100%",
-  },
-  ingredientRow: {
-    display: "flex",
-    alignItems: "center",
-    marginBottom: "10px",
-    width: "100%",
-  },
-  ingredientInput: {
-    flex: "1",
-    padding: "8px",
-    marginBottom: "5px",
-    borderRadius: "4px",
-    border: "1px solid #ccc",
-    backgroundColor: "#f9f9f9",
-  },
-  amountInput: {
-    flex: "1",
-    marginLeft: "10px",
-    padding: "8px",
-    borderRadius: "4px",
-    border: "1px solid #ccc",
-  },
-  form: {
-    display: "flex",
-    flexDirection: "column",
-  },
-  formGroup: {
-    marginBottom: "20px",
-    width: "100%",
-  },
-  label: {
-    fontSize: "16px",
-    marginBottom: "8px",
-    display: "block",
-  },
-  input: {
-    width: "100%",
-    padding: "8px",
-    marginBottom: "10px",
-    borderRadius: "4px",
-    border: "1px solid #ccc",
-  },
-  textarea: {
-    width: "100%",
-    height: "150px",
-    padding: "8px",
-    borderRadius: "4px",
-    border: "1px solid #ccc",
-  },
-  fileInput: {
-    width: "100%",
-    padding: "8px",
-  },
-  buttonGroup: {
-    display: "flex",
-    justifyContent: "flex-end",
-    marginTop: "10px",
-  },
-  submitButton: {
-    padding: "8px 16px",
-    backgroundColor: "#28a745",
-    color: "#fff",
-    border: "none",
-    borderRadius: "4px",
-    cursor: "pointer",
-    marginRight: "10px",
-  },
-  cancelButton: {
-    padding: "8px 16px",
-    backgroundColor: "#dc3545",
-    color: "#fff",
-    border: "none",
-    borderRadius: "4px",
-    cursor: "pointer",
-  },
-  addButton: {
-    padding: "8px 16px",
-    backgroundColor: "#007bff",
-    color: "#fff",
-    border: "none",
-    borderRadius: "4px",
-    cursor: "pointer",
-    marginTop: "10px",
-  },
-};
 
 export default MyCocktail;
