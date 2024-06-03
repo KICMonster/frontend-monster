aba6b95695a6075aaa3fa2eec1a628bd0b32801d
user\KITCOOP <dbaudwns24@naver.com>
2024-06-03 13:43:49 +0900

    chartjsx

--- front/src/component/detail/CocktailSearchChart.jsx
+++ front/src/component/detail/CocktailSearchChart.jsx
@@ -28,19 +28,18 @@ ChartJS.register(
 
 // axios 인스턴스 생성
 const axiosInstance = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL // 백엔드 주소
   baseURL: import.meta.env.VITE_API_URL
 });
 
 const CocktailSearchChart = () => {
-  const [timeSlotLogs, setTimeSlotLogs] = useState([]);
+  const [viewLogs, setViewLogs] = useState([]);
   const [timeRange, setTimeRange] = useState('today'); // 기본값: 오늘
-  const [selectedHour, setSelectedHour] = useState(null); // 선택된 시간대를 상태로 추가
+  const [category, setCategory] = useState('cocktail'); // 기본값: 칵테일
   const [isDataEmpty, setIsDataEmpty] = useState(false); // 데이터가 비어있는지 확인하는 상태 추가
 
   useEffect(() => {
     fetchData();
-  }, [timeRange]); // timeRange가 변경될 때마다 데이터 다시 가져오기
+  }, [timeRange, category]); // 카테고리 변경에도 반응하도록 useEffect 수정
 
   const fetchData = async () => {
     try {
@@ -65,18 +64,20 @@ const CocktailSearchChart = () => {
       // 서버에 시간 범위 데이터 보내기
       const response = await axiosInstance.post('/search/chart', {
         start: startDate,
-        end: currentDate.toISOString() // 현재 시간을 UTC로 변환하여 전달
+        end: currentDate.toISOString(),
+        category,
+        gender: null,
+        birth: null
       });
 
-      // 시간 데이터를 로컬 시간으로 변환
-      const localTimeSlotLogs = response.data.map(slot => ({
-        ...slot,
-        hour: convertUTCToLocalHour(slot.hour)
+      console.log(response.data);
+      const logs = response.data.map(log => ({
+        ...log,
+        viewDate: new Date(log.viewDate).toLocaleString()
       }));
 
-      setTimeSlotLogs(localTimeSlotLogs);
-      setIsDataEmpty(localTimeSlotLogs.length === 0); // 데이터가 비어있는지 확인
-      console.log(localTimeSlotLogs);
+      setViewLogs(logs);
+      setIsDataEmpty(logs.length === 0); // 데이터가 비어있는지 확인
     } catch (error) {
       console.error('Error fetching search logs:', error);
       setIsDataEmpty(true); // 오류 발생 시 데이터가 비어있는 것으로 간주
@@ -102,18 +103,12 @@ const CocktailSearchChart = () => {
     return startOfThisMonth.toISOString();
   };
 
-  const convertUTCToLocalHour = (utcHour) => {
-    const date = new Date();
-    date.setUTCHours(utcHour, 0, 0, 0);
-    return date.getHours();
-  };
-
   const chartData = {
-    labels: timeSlotLogs.map(log => `${log.hour}:00`), // 시간대를 라벨로 사용
+    labels: viewLogs.map(log => log.viewDate), // 시간대를 라벨로 사용
     datasets: [
       {
         label: '칵테일 검색 횟수',
-        data: timeSlotLogs.map(log => log.searchCount),
+        data: viewLogs.map(log => 1), // 각 로그는 하나의 검색으로 간주
         fill: false,
         borderColor: 'rgb(13, 12, 34)', // 연보라색으로 변경
         tension: 0.1
@@ -129,78 +124,7 @@ const CocktailSearchChart = () => {
           stepSize: 1 // y축 눈금을 1 단위로 설정
         }
       }
-    },
-    onClick: function (event, elements) {
-      if (elements.length > 0) {
-        const clickedHour = chartData.labels[elements[0].index].split(':')[0];
-        handleHourClick(parseInt(clickedHour)); // 시간대 클릭 시 해당 시간대 선택
-      } else {
-        setSelectedHour(null); // 선택된 시간대가 없을 때 전체 검색
-        fetchData(); // 데이터 다시 가져오기
-      }
     }
-  };
-
-  const handleHourClick = (hour) => {
-    if (selectedHour === hour) {
-      setSelectedHour(null); // 이미 선택된 시간대를 다시 클릭하면 선택 해제
-    } else {
-      setSelectedHour(hour); // 클릭한 시간대를 선택
-    }
-  };
-
-  const getSelectedRankings = () => {
-    if (selectedHour !== null) {
-      return getRankings(selectedHour);
-    } else {
-      return getTotalRankings(); // 선택된 시간대가 없으면 총 검색 순위 반환
-    }
-  };
-
-  const getRankings = (hour) => {
-    const cocktailCounts = {};
-
-    timeSlotLogs
-      .filter(log => log.hour === hour)
-      .forEach(log => {
-        log.views.forEach(view => {
-          const cocktailName = view.name;
-          if (cocktailCounts[cocktailName]) {
-            cocktailCounts[cocktailName]++;
-          } else {
-            cocktailCounts[cocktailName] = 1;
-          }
-        });
-      });
-
-    const sortedCocktails = Object.entries(cocktailCounts).sort((a, b) => b[1] - a[1]);
-
-    return sortedCocktails.slice(0, 10); // 최대 10개의 순위만 반환
-  };
-
-  const getTotalRankings = () => {
-    const cocktailCounts = {};
-
-    timeSlotLogs.forEach(log => {
-      log.views.forEach(view => {
-        const cocktailName = view.name;
-        const cocktailId = view.id; // 칵테일 id 추출
-        if (cocktailCounts[cocktailName]) {
-          cocktailCounts[cocktailName].count++;
-        } else {
-          cocktailCounts[cocktailName] = {
-            count: 1,
-            id: cocktailId // 칵테일 id 저장
-          };
-        }
-      });
-    });
-
-    const sortedCocktails = Object.entries(cocktailCounts)
-      .sort((a, b) => b[1].count - a[1].count)
-      .map(([name, { count, id }]) => [name, count, id]); // 이름, 카운트, id 반환
-
-    return sortedCocktails.slice(0, 10);
   };
 
   return (
@@ -211,9 +135,14 @@ const CocktailSearchChart = () => {
           <button onClick={() => setTimeRange('today')}>오늘</button>
           <button onClick={() => setTimeRange('thisWeek')}>이번 주</button>
           <button onClick={() => setTimeRange('thisMonth')}>이번 달</button>
+          {/* 카테고리 선택 추가 */}
+          <select value={category} onChange={e => setCategory(e.target.value)}>
+            <option value="cocktail">Cocktail</option>
+            <option value="customCocktail">Custom Cocktail</option>
+          </select>
         </div>
       </div>
-      <div className="chart" >
+      <div className="chart">
         <Line data={chartData} options={options} />
       </div>
       {isDataEmpty && (
@@ -223,40 +152,8 @@ const CocktailSearchChart = () => {
           <button onClick={() => setTimeRange('thisMonth')}>이번 달</button>
         </div>
       )}
-      {!isDataEmpty && (
-        <div className="ranking-container">
-          <h3>검색 순위</h3>
-          {selectedHour !== null && (
-            <>
-              <h4>{selectedHour}시 순위</h4>
-              <div className="ranking-list">
-                {getSelectedRankings().map(([name, count], index) => (
-                  <div className="ranking-item" key={index}>
-                    <span className="ranking-position">{index + 1}</span>
-                    <span className="ranking-name">{name}</span>
-                  </div>
-                ))}
-              </div>
-            </>
-          )}
-          {selectedHour === null && (
-            <>
-              <h4>오늘 총 검색 순위</h4>
-              <div className="ranking-list">
-                {getSelectedRankings().map(([name, count, id], index) => (
-                  <div className="ranking-item" key={index}>
-                    <span className="ranking-position">{index + 1}</span>
-                    <Link to={`/cocktail/${id}`} className="ranking-name">{name}</Link>
-                  </div>
-                ))}
-              </div>
-            </>
-          )}
-        </div>
-      )}
     </BasicLayout>
   );
 };
 
-
-export default CocktailSearchChart;
+export default CocktailSearchChart; 
